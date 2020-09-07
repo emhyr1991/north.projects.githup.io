802576179077457b93bafa4a86d75400c2b3af25
Radyslav Dmytrenko <1991barbossa@gmail.com>
2020-09-07 09:55:33 +0300

    to make some nice effects for different elements

---
+++ app/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider1').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        speed: 1000,
+        easing: 'linear',
+
+    });
+});
